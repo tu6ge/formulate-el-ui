4011263cad1bbac27db1a12bb8ed73cda9319c61
tu6ge <772364230@qq.com>
2021-06-18 14:00:34 +0800

    预览

---
+++ src/components/specimens/SpecimenButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="specimens">
+    <h2>Button classification</h2>
+    <div class="specimen">
+      <h3>Button</h3>
+      <FormulateInput
+        label="Click me"
+        type="button"
+      />
+    </div>
+    <div class="specimen">
+      <h3>Submit</h3>
+      <FormulateInput
+        label="Submit me"
+        type="submit"
+      />
+    </div>
+    <div class="specimen">
+      <h3>Submit</h3>
+      <FormulateInput
+        disabled
+        label="Submit me"
+        type="submit"
+      />
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import VueFormulate from '@braid/vue-formulate'
+import FormulateSpecimens from './components/FormulateSpecimens.vue'
+import zh from './i18n/zh.js'
 
 Vue.config.productionTip = false
 
+const ElThem = function (instance) {
+  instance.extend({
+    classes: {
+      outer: 'el-formulate-input'
+    }
+  })
+}
+
+Vue.use(VueFormulate, {
+  plugins: [zh, ElThem],
+  locale: 'zh'
+})
+
 new Vue({
-  render: h => h(App),
+  render: h => h(FormulateSpecimens),
 }).$mount('#app')
