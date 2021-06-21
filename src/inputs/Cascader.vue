b727098bbdc1817233f4ad078b3d3a9d98fcf412
tu6ge <772364230@qq.com>
2021-06-21 11:59:35 +0800

    feat: 添加级联选择器组件

---
+++ src/inputs/Cascader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    :class="context.classes.element"
+    :data-type="context.type"
+  >
+    <el-cascader 
+      v-model="context.model"
+      v-bind="context.slotProps.component"
+      @change="context.rootEmit('change', $event)"
+      @expand-change="context.rootEmit('expand-change', $event)"
+      @visible-change="context.rootEmit('visible-change', $event)"
+      @blur="context.blurHandler"
+      @focus="context.rootEmit('focus', $event)"
+    />
+  </div>
+</template>
+<script>
+import {Cascader} from 'element-ui'
+export default {
+  props: {
+    context: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    [Cascader.name]: Cascader
+  }
+}
+</script>
--- src/inputs/index.js
+++ src/inputs/index.js
@@ -1,14 +1,24 @@
-import MyInput from './Input.vue'
 import Vue from 'vue'
 
-Vue.component('MyInput', MyInput)
+import FeCascader from './Cascader.vue'
+import FeInput from './Input.vue'
+
+Vue.component('FeCascader', FeCascader)
+Vue.component('FeInput', FeInput)
 
 export default function (instance) {
   instance.extend({
     library: {
+      'el-cascader': {
+        classification: 'text',
+        component: 'FeCascader',
+        slotProps: {
+          component: ['clearable', 'options', 'props', 'size', 'placeholder', 'disabled', 'showAllLevels', 'collapseTags', 'separator', 'filterMethod', 'debounce', 'beforeFilter', 'popperClass']
+        }
+      },
       'el-input': {
         classification: 'text',
-        component: 'MyInput',
+        component: 'FeInput',
         slotProps: {
           component: ['clearable', 'showWordLimit', 'maxlength', 'minlength']
         }
