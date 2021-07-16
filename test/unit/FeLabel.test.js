296fddbe2c666244207e0bb2749005abea92506d
tu6ge <772364230@qq.com>
2021-07-16 23:35:51 +0800

    test(init): 初始化 jest vue 组件

--- test/unit/FeLabel.test.js
+++ test/unit/FeLabel.test.js
@@ -1,5 +1,4 @@
-import { mount } from '@vue/test-utils'
-import Vue from 'vue'
+import { createLocalVue, mount } from '@vue/test-utils'
 import Formulate from '@braid/vue-formulate'
 import FeLabel from '@/slots/FeLabel.vue'
 
@@ -14,17 +13,57 @@ const options = {
     label: ['requiredTip']
   }
 }
-Vue.use(Formulate, options)
-
-const resetInstance = () => {
-  Formulate.install(Vue, options)
-}
+const localVue = createLocalVue()
+localVue.use(Formulate, options)
 
 describe('FeLabel', () => {
-  it('sets unknown classification if not in library', () => {
-    const wrapper = mount(FeLabel, { propsData: {
-      type: 'foobar',
-    } })
-    expect(wrapper.vm.classification).toBe('unknown')
+  it('test find feLabel Component', () => {
+    const wrapper = mount(Formulate.options.components.FormulateInput, {
+      localVue,
+      propsData: {
+        type: 'text',
+        label:'姓名'
+      }
+    })
+    expect(wrapper.findComponent(FeLabel).exists()).toBe(true)
+  })
+
+  it('test label default class', () => {
+    const wrapper = mount(Formulate.options.components.FormulateInput, {
+      localVue,
+      propsData: {
+        type: 'text',
+        label:'姓名'
+      }
+    })
+    const label = wrapper.find('.formulate-input-label');
+    expect(label.classes()).toEqual(['formulate-input-label', 'formulate-input-label--before'])
+  })
+
+  it('test label class when it have requried rule', () => {
+    const wrapper = mount(Formulate.options.components.FormulateInput, {
+      localVue,
+      propsData: {
+        type: 'text',
+        label:'姓名',
+        validation: ['required']
+      }
+    })
+    const label = wrapper.find('.formulate-input-label');
+    expect(label.classes()).toEqual(['formulate-input-label', 'formulate-input-label--before', 'formulate-input-label--required'])
+  })
+
+  it('test label class when it have requried rule , and force hidden', () => {
+    const wrapper = mount(Formulate.options.components.FormulateInput, {
+      localVue,
+      propsData: {
+        type: 'text',
+        label:'姓名',
+        validation: ['required'],
+        requiredTip: false
+      }
+    })
+    const label = wrapper.find('.formulate-input-label');
+    expect(label.classes()).toEqual(['formulate-input-label', 'formulate-input-label--before'])
   })
 })
