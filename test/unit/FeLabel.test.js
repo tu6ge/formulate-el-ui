5c0fcc0f9978eec161a96868fd08afc8328f9536
tu6ge <772364230@qq.com>
2021-07-16 18:04:48 +0800

    test: felabel 未完成

--- test/unit/FeLabel.test.js
+++ test/unit/FeLabel.test.js
@@ -1,2 +1,30 @@
 import { mount } from '@vue/test-utils'
+import Vue from 'vue'
+import Formulate from '@braid/vue-formulate'
 import FeLabel from '@/slots/FeLabel.vue'
+
+const options = {
+  components:{
+    FeLabel,
+  },
+  slotComponents: {
+    label: 'FeLabel'
+  },
+  slotProps: {
+    label: ['requiredTip']
+  }
+}
+Vue.use(Formulate, options)
+
+const resetInstance = () => {
+  Formulate.install(Vue, options)
+}
+
+describe('FeLabel', () => {
+  it('sets unknown classification if not in library', () => {
+    const wrapper = mount(FeLabel, { propsData: {
+      type: 'foobar',
+    } })
+    expect(wrapper.vm.classification).toBe('unknown')
+  })
+})
